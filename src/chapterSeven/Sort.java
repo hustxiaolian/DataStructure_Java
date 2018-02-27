e697bea63596c07a124a3cdb1febc570a410f9c8
hustxiaolian <2504033134@qq.com>
2018-02-27 21:55:50 +0800

    add quickSort

--- src/chapterSeven/Sort.java
+++ src/chapterSeven/Sort.java
@@ -7,6 +7,7 @@ public final class Sort {
 	 * 按照从小到大的顺序进行插入排序
 	 * 插入排序基于，在数组位置0-i的子数组已经完成排序，因此位置i+1的元素只需要依次向前比对，插入到前面子数组合适的位置即可
 	 * 时间界限为O（N^2）
+	 * 实际测试：十万级：20000ms
 	 * @param arr 需要排序的数组
 	 */
 	public static <AnyType extends Comparable<? super AnyType>> void insertionSort(AnyType[] arr) {
@@ -17,10 +18,10 @@ public final class Sort {
 			AnyType nowItem = arr[nowPos];
 			
 			//向前比对，寻求插入的合适位置
-			for(insertionPos = nowPos - 1;insertionPos >= 0 && 
-					arr[insertionPos].compareTo(arr[insertionPos + 1]) > 0;--insertionPos) {
+			for(insertionPos = nowPos;insertionPos > 0 && 
+					nowItem.compareTo(arr[insertionPos - 1]) < 0;--insertionPos) {
 				
-				arr[insertionPos + 1] =  arr[insertionPos];
+				arr[insertionPos] =  arr[insertionPos - 1];
 			}
 			//插入
 			arr[insertionPos] = nowItem;
@@ -33,6 +34,8 @@ public final class Sort {
 	 * 思路步骤：
 	 * 1. 首先必须有建立，或者外部传入一个增量序列，其第一个值必须为1.
 	 * 2. 再按照插入排序的思路，进行hk排序。
+	 * 
+	 * 实际测试：十万级：86ms
 	 * @param arr 需要排序的数组
 	 */
 	public static <AnyType extends Comparable<? super AnyType>> void shellSort(AnyType[] arr) {
@@ -43,7 +46,7 @@ public final class Sort {
 			for(int i = gap;i < arr.length; ++i) {
 				AnyType nowItem = arr[i];
 				
-				for(j = i; j > 0 && arr[j].compareTo(arr[j - gap]) < 0; --j) {
+				for(j = i; j - gap >= 0 && nowItem.compareTo(arr[j - gap]) < 0; j -= gap) {
 					arr[j] = arr[j - gap];
 				}
 				
@@ -57,6 +60,8 @@ public final class Sort {
 	 * 思路：
 	 * 1. 首先使用传入的数组进行下滤操作，建立最大（最小）二叉堆。
 	 * 2. 使用二叉堆的deleteMin操作（这个操作中包含又一次的下滤操作），进行多次，并且把返回值放入二叉堆使用数组的后面，节省空间。
+	 * 
+	 * 实际测试：十万级：
 	 * 
 	 */
 	public static <AnyType extends Comparable<? super AnyType>> void binaryHeapSort(AnyType[] arr) {
@@ -106,7 +111,7 @@ public final class Sort {
 	 * @param j 数组坐标
 	 * @throws java.lang.IndexOutOfBoundsException 传入坐标越界
 	 */
-	private static <AnyType> void arraySwap(AnyType[] arr,int i,int j) {
+	private static final <AnyType> void arraySwap(AnyType[] arr,int i,int j) {
 		AnyType temp = arr[i];
 		arr[i] = arr[j];
 		arr[j] = temp;
@@ -123,6 +128,8 @@ public final class Sort {
 	 * 
 	 * 同时，值得注意的是，这里有一个不起眼但是很重要的亮点。就是事先就声明了一个等长度
 	 * 的暂时数组，用作排序过程中的临时缓冲区，
+	 * 
+	 * 实际测试：十万级：116ms
 	 */
 	@SuppressWarnings("unchecked")
 	public static <AnyType extends Comparable<? super AnyType>> void mergeSort(AnyType[] arr) {
@@ -190,10 +197,122 @@ public final class Sort {
 	}
 	
 	/**
-	 * 传说中大名鼎鼎的快排！
+	 * 经典快速排序。将数组S排序的基本算法由下列简单的四步组成：
+	 * 1. 如果S中元素的个数为0或者1，则直接返回
+	 * 2. 取S中任一元素v,称之为枢纽元（pivot）。
+	 * 3. 将S按照v划分为两个不相交的集合：S1（小）和S2（大）
+	 * 4. 返回quickSort(S1)后跟v，继而返回quickSort(S2)
+	 * 
+	 * 快速排序和归并排序都是一种分治策略产生的递归算法。不同于归并排序将一个数组递归地分为几乎相等的两部分。
+	 * 快速排序在数组中选取合适的枢纽元，将数组分割成两个可能不想等的两个不相交集合，其中一个集合的元素比枢纽元大。
+	 * 另外一个集合的元素都比枢纽元小。
+	 * 
+	 * 快速排序比归并排序更快的原因：
+	 * 在数组合适的位置递归地分割数组会产生意想不到的高效，这种高效不仅仅能够弥补不均衡分割产生的不足，还能在性能有所超出。
+	 * 
+	 * 因此，合理选择枢纽元，分割数组显得尤为重要。
+	 * 几种错误的枢纽元选择方式：
+	 * 1. 选择第一个元素作为枢纽元->碰到预排序（很常见）和反序的就傻逼了。
+	 * 2. 选择前两个互异的关键字中的较大者作为枢纽元->同上，碰上预排序和反序就相当尴尬了，时间界限直接变成了二次方。
+	 * 
+	 * 一种安全但是效率较低的方式：
+	 * 1. 使用随机数随机选取枢纽元->随机数的生成开销挺大的，无法降低算法其余部分的运行时间。
+	 * 
+	 * 推荐的分割策略：三数中值分割法，使用左端，中间和右端三个元素的中值作为枢纽元。
+	 * 
+	 * 确定枢纽元后，接下俩的如何将数组的元素划分到枢纽元的两边，并且不产生额外的内存占用。
+	 * 1.先把枢纽元移动到数组的最后。使得枢纽元离开待分割的区域。
+	 * 2.令i = 0，j = arr.length - 2。当i指向的元素小于枢纽元，i向右移动掠过，同理，当j指向的元素大于枢纽元时，j向左移动。
+	 * 3.反之，当i指向的元素大于枢纽元，i停下，当j指向的元素小于枢纽元，j停下。然后交换i，j指向的元素。
+	 * 4.当i > j时，停下，交换此时位置i和数组最后的元素。
+	 * 
+	 * 前面的步骤忽略了一个重要的问题，即如何处理与枢纽元相同的元素。
+	 * 想一下极端的情况，如果数组都是相同的元素。如果当i或者j等于枢纽元的时候停下，则会产生看起来多余的交换操作。但是，如果我们
+	 * 不停下，那么就必须要指定一个防止i，j出界的策略。同时，这样分割产生的集合会产生两个非常不均衡的集合。因此，我们选择
+	 * 使用一些额外的操作换取对风险的规避。
+	 * 
+	 * 最后，当处理N=10左右的小数组，直接选择插入排序好得多。
+	 * 
+	 * 
 	 * @param arr
 	 */
+	private static final int CUTOFF = 10;//不再进行快速排序的数组大小
+	
 	public static <AnyType extends Comparable<? super AnyType>> void quickSort(AnyType[] arr) {
+		quickSort(arr, 0, arr.length - 1);
+	}
+	
+	/**
+	 * 快排的主例程
+	 * @param arr
+	 * @param left
+	 * @param right
+	 */
+	private static <AnyType extends Comparable<? super AnyType>> void quickSort(AnyType[] arr,int left,int right) {
+		//没到截断范围时，直接快排
+		if(left + CUTOFF <= right) {
+			//三只分割法选择枢纽元
+			AnyType pivot = median3Split(arr, left, right);
+			
+			int i = left;
+			int j = right - 1;
+			while(true) {
+				while( arr[++i].compareTo(pivot) < 0) {}
+				while( arr[--j].compareTo(pivot) > 0) {}
+				
+				if(i < j) {
+					AnyType temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+				else {
+					break;
+				}
+			}
+			//把枢纽元交换会正确的位置
+			arraySwap(arr, i, right - 1);
+			
+			quickSort(arr, left, i - 1);
+			quickSort(arr, i + 1, right);
+		}
+		else {
+			//小数组情况直接调用插入排序快得多
+			insertionSort(arr);
+		}
+	}
+	
+	/**
+	 * 三值分割算法.顺便已经完成了这个元素的排序。节省了一步。相当于只需要对left + 1，以及right - 1进行分割。
+	 * 因此，按照前面描述的策略，我们可以将枢纽元放在right - 1的位置上，而不是right上。
+	 * 同时，最左边和左右边也会成为一种警戒标志，i和j一定不要出现数组访问越界这种尴尬的情况，这真是相当妙的算法
+	 * @param arr
+	 * @param left
+	 * @param right
+	 * @return
+	 */
+	private static <AnyType extends Comparable<? super AnyType>> AnyType median3Split(AnyType[] arr,int left,int right) {
 		
+		int center = (left + right) / 2;
+		
+		if(arr[left].compareTo(arr[center]) > 0) {
+			AnyType temp = arr[left];
+			arr[left] = arr[center];
+			arr[center] = temp;
+		}
+		if(arr[left].compareTo(arr[right]) > 0) {
+			AnyType temp = arr[left];
+			arr[left] = arr[right];
+			arr[right] = temp;
+		}
+		if(arr[center].compareTo(arr[right]) > 0) {
+			AnyType temp = arr[center];
+			arr[center] = arr[right];
+			arr[right] = temp;
+		}
+		//
+		AnyType temp = arr[center];
+		arr[center] = arr[right - 1];
+		arr[right - 1] = temp;
+		return arr[right - 1];
 	}
 }
---
+++ src/chapterSeven/SortTest.java
@@ -0,0 +1,29 @@
+package chapterSeven;
+
+public class SortTest {
+
+	public static void main(String[] args) {
+		Integer[] arr = generateMillionRandomIntNum(1000,100000);
+		//Integer[] arr = {1,6,5,8,9,7,11,20,96,3,2,11,55,22,99,88,77,20,57,87,30};
+		long start = System.currentTimeMillis();
+		Sort.quickSort(arr);
+		long end1 = System.currentTimeMillis();
+		System.out.println("time1:" + (end1 - start));
+		/*
+		for (Integer integer : arr) {
+			System.out.println(integer + ",");
+		}
+		*/
+		
+	}
+	
+	public static Integer[] generateMillionRandomIntNum(int up,int size) {
+		Integer[] arr = new Integer[size];
+		
+		for(int i = 0;i <arr.length;++i) {
+			arr[i] = (int)(Math.random() * up);
+		}
+		
+		return arr;
+	}
+}
